b9937ae1c4ee72db2bc4b2934f84a5bed9ef961c
Carmine Faella <carminefaella5@gmail.com>
2023-04-11 12:49:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script>
+
+import PageHeader from './components/PageHeader.vue';
+import PageMain from './components/PageMain.vue';
+import PageFooter from './components/PageFooter.vue';
+
+export default{
+  components:{
+    PageHeader,
+    PageMain,
+    PageFooter
+  },
+  data(){
+    return{
+      navBar:[
+                {
+                    name:'Home',
+                    href:'#home'
+                },
+                {
+                    name:'Pages',
+                    href:'#Pages'
+                },
+                {
+                    name:'Portfolio',
+                    href:'#Portfolio'
+                },
+                {
+                    name:'Blog',
+                    href:'#Blog'
+                },
+                {
+                    name:'Shop',
+                    href:'#Shop'
+                },
+                {
+                    name:'Elements',
+                    href:'#Elements'
+                },
+            ]
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <header>
+    <PageHeader :nav="navBar"/>
+  </header>
+
+  <main>
+    <PageMain/>
+  </main>
+
+  <footer>
+    <PageFooter/>
+  </footer>
+  
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,55 @@
+<script>
+
+export default{
+
+    data(){
+        return{
+            
+        }
+    },
+    props:{
+        nav:Array
+    }
+
+}
+
+</script>
+
+<template>
+
+    <div class="topbar">
+
+        <div>
+            <img src="../assets/logo-sidearea-1.png" alt="Logo header">
+        </div>
+        <nav>
+            <li v-for="name in nav"><a :href="name.href">{{ name.name }}</a></li>
+        </nav>
+
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+@use '../styles/general.scss' as *;
+@use '../styles/partial/variables.scss' as *;
+
+.topbar{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+
+    li{
+        display: inline-block;
+        padding: 0 1rem;
+        list-style: none;
+
+        a{
+            text-decoration: none;
+            color: $text-color-11;
+        }
+    }
+}
+
+</style>
