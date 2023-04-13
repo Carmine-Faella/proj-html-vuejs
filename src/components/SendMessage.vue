d5d95de700bf1031f0aaa92612d74cb59c2fafd4
Carmine Faella <carminefaella5@gmail.com>
2023-04-13 10:20:52 +0200

    Inserimento immagini di sfondo

--- src/components/SendMessage.vue
+++ src/components/SendMessage.vue
@@ -18,7 +18,19 @@ export default{
             <h1>Want to work with us? Send us <span>a message!</span></h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus aut animi ipsa sit sunt, modi minus vero tenetur, veniam sint voluptates molestias beatae, dignissimos rem quisquam asperiores recusandae odit unde.</p>
             <button>CONTACT</button>
-            <img src="../assets/svg-.svg" alt="">
+        </div>
+
+        <div class="img-back">
+            <img src="../assets/short-slider-rev-1-img-2.png" alt="immagine sfondo" class="img-flower">
+            <img src="../assets/short-slider-rev-1-img-6.png" alt="immagine sfondo" class="img-flower2">
+            <img src="../assets/short-slider-rev-1-img-1.png" alt="immagine sfondo" class="img-tree">
+            <img src="../assets/short-slider-rev-1-img-4.png" alt="immagine sfondo" class="img-cloud">
+            <img src="../assets/short-slider-rev-1-img-4.png" alt="immagine sfondo" class="img-cloud2">
+            <img src="../assets/short-slider-rev-1-img-5.png" alt="immagine sfondo" class="img-cloud3">
+            <img src="../assets/short-slider-rev-1-img-5.png" alt="immagine sfondo" class="img-cloud4">
+            <img src="../assets/short-slider-rev-1-img-4.png" alt="immagine sfondo" class="img-cloud5">
+            <img src="../assets/short-slider-rev-1-img-4.png" alt="immagine sfondo" class="img-cloud6">
+            <img src="../assets/short-slider-rev-1-img-9.png" alt="immagine sfondo" class="img-tree2">
         </div>
 
     </section>
@@ -32,7 +44,8 @@ export default{
 
 section{
     background-color: $back-6;
-    padding: 5rem 0
+    padding: 5rem 0;
+    position:relative
 }
 .container{
     @include my-container;
@@ -59,6 +72,81 @@ section{
     }
 }
 
+.img-back{
+    img{
+        position: absolute;
+        filter: opacity(0.5);
+    }
+
+    .img-cloud{
+        top: 0;
+        transform: rotate(180deg);
+        width: 200px;
+        height: 80px;
+        left: -50px;
+    }
+
+    .img-cloud3{
+        top: 0;
+        left: 200px;
+        transform: rotate(180deg);
+        width: 150px;
+    }
+
+    .img-cloud2{
+        top: 0;
+        transform: rotate(180deg);
+        transform: rotateX(180deg);
+        width: 200px;
+        height: 80px;
+        right: -50px;
+    }
+
+    .img-cloud4{
+        top: 0;
+        right: 200px;
+        transform: rotate(180deg);
+        transform: rotateX(180deg);
+        width: 150px;
+    }
+
+    .img-cloud5{
+        bottom: 0;
+        right: 15%;
+        width: 250px;
+        height: 70px;
+    }
+
+    .img-cloud6{
+        bottom: 0;
+        left: 20%;
+        width: 100px;
+        height: 40px;
+        transform: rotatey(180deg);
+    }
+
+    .img-tree{
+        right: 0;
+        bottom: 0;
+        width: 150px;
+    }
+
+    .img-tree2{
+        left: 0;
+        bottom: 0;
+    }
+
+    .img-flower{
+        top: 100px;
+        right: 150px;
+    }
+
+    .img-flower2{
+        bottom: 20px;
+        left: 200px;
+    }
+    
+}
 
 
 </style>
